76cdf8ee1354eb39769e5fa469c08ffa77d7a17a
Funloi2 <Charlesmarguerite11@gmail.com>
2023-10-11 11:03:53 +0200

    finalisation de job details

--- front-end/src/Components/JobDetail.js
+++ front-end/src/Components/JobDetail.js
@@ -1,25 +1,24 @@
 
 
-function JobDetail(jobDetail){
+function JobDetail({jobDetail}){
 
 
     return(
-        <div>
-            <div className="row text-center">
-                <h1>{jobDetail.name}</h1>
-            </div>
+        <div className="jobCard">
+            <h1 className="text-center mb-5">{jobDetail.name}</h1>
             <div className="row">
-                <div className="col-6">
+                <div className="col me-5 pe-5">
                     <p><strong>Company:</strong> {jobDetail.company}</p>
                     <p><strong>Salary:</strong> {jobDetail.salary}</p>
                     <p><strong>Contract:</strong> {jobDetail.contract}</p>
                 </div>
-                <div className="col-6">
+                <div className="col ms-5 ps-5">
                     <p><strong>City:</strong> {jobDetail.city}</p>
                     <p><strong>Type:</strong> {jobDetail.type}</p>
                     <p><strong>Date de publication:</strong> {jobDetail.date}</p>
                 </div>
             </div>
+            <p><strong>Description:</strong> {jobDetail.description}</p>
         </div>
     );
 
