2df91089b1f9cc3112cdf7c8d4eb5e29638d02cb
DorianContal <dorian.contal@epitech.eu>
2023-10-13 11:21:15 +0200

    BacKEnd JobDetails fonctionnel

--- back-end/Controlers/Advertisement.js
+++ back-end/Controlers/Advertisement.js
@@ -12,20 +12,20 @@ export const getAllAdvertissment = async (req, res) => {
     }
 }
 export const getAdvertisementById = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const result = await pool.query("SELECT * FROM Advertisement WHERE idAd = ?", [id]);
-        
-      console.log(result);
-      
-      if (result.length > 0) {
-        res.status(200).json({ data: result[0] });
-      } else {
-        res.status(404).json({ message: "Annonce non trouvée" });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(400).json({ message: "Requête échouée" });
+  try {
+    const { id } = req.params;
+
+    const query = "SELECT * FROM Advertisement WHERE idAd = ?";
+    const result = await pool.query(query, [id]);
+    
+    if (result.length > 0) {
+      res.status(200).json({ data: result[0] }); // Utilisez result.rows[0] pour obtenir la première ligne de résultat.
+    } else {
+      res.status(404).json({ message: "Annonce non trouvée" });
     }
-  };
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Requête échouée" });
+  }
+};
   
--- back-end/routes/advertisement.js
+++ back-end/routes/advertisement.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getAdvertisementById, getAllAdvertissment } from "./../Controlers/Advertisement.js";
+import {getAllAdvertissment } from "./../Controlers/Advertisement.js";
+import { getAdvertisementById} from "./../Controlers/Advertisement.js";
 
 const router = express.Router();
 
 router.get('/', getAllAdvertissment);
-router.get('/advertisement/:id',getAdvertisementById);
+router.get('/:id',getAdvertisementById);
 
 export default router;
